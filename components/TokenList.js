29d49ae3b7c843df9b9bf9b53ef9b1b4041f42cb
Ajand Mardalizad <aseedgeek@gmail.com>
2022-08-19 16:22:39 +0430

    token factory template added

---
+++ components/TokenList.js
@@ -0,0 +1,77 @@
+/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  Container,
+  Button,
+} from "@mui/material";
+import { useRouter } from "next/router";
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+const TokenList = () => {
+  const router = useRouter();
+
+  return (
+    <Container
+      css={css`
+        padding-top: 5em;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          justify-content: flex-end;
+          margin-bottom: 1em;
+        `}
+      >
+        <Button onClick={() => router.push('/create-token')} variant="text" color="primary">
+          Create A Governance Token
+        </Button>
+      </div>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Contract Address</TableCell>
+              <TableCell>Token Name</TableCell>
+              <TableCell>Symbol</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.calories}</TableCell>
+                <TableCell>{row.fat}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
+
+export default TokenList;
